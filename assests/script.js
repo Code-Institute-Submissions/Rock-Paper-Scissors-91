4c33e18d054f3c3ad379daa119959f54eb71a3b4
Joannalsk <joanna.skennedy@yahoo.co.uk>
2023-01-19 19:57:05 +0000

    add java for game area

--- assests/script.js
+++ assests/script.js
@@ -1,21 +1,61 @@
-const computerChoice = document.getElementById('computer-choice')
-const userChoiceDisplay = document.getElementById('your-choice')
-const result = document.getElementById('result')
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('computer-choice')
+
+let userChoice
+let computerChoice
+let result
 
 const possibleChoices = document.querySelectorAll('button')
 
-let userChoice
-
-possibleChoices.forEach(possibleChoices=> possibleChoices.addEventListener ('click',(e)=>{
+possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
-    generateComputerChoice ()
+    generateComputerChoice()
+    getResult
+
 }))
 
 function generateComputerChoice() {
-    const randomNumber = Math.floor (Math.random() * 3)
-    console.log (randomNumber)
+    const randomNumber = Math.floor(Math.random() * 3)
+    console.log(randomNumber)
+
+    if (randomNumber === 1) {
+        computerChoice = 'rock'
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'paper'
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'scissors'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
 }
 
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = 'Draw!'
+    }
 
+    if (computerChoice === 'rock' && userChoice === 'scissors') {
+        result = 'Computer wins!'
+    }
+    if (computerChoice === 'scissors' && userChoice === 'rock') {
+        result = 'Player wins!'
+    }
+    if (computerChoice === 'scissors' && userChoice === 'paper') {
+        result = 'Computer wins!'
+    }
+     if (computerChoice === 'paper' && userChoice === 'scissors') {
+            result = 'Player wins!'
+    }
+    
+    if (computerChoice === 'rock' && userChoice === 'paper') {
+        result = 'Player wins!'
+    }
 
+    if (computerChoice === 'paper' && userChoice === 'rock') {
+        result = 'Computer wins!'
+    }
+    resultDisplay.innerHTML = result
+}
